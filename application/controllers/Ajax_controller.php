0952125cefd922f6972464bc7c962f591bb5813e
summery <hellosummer0604@gmail.com>
2015-12-25 17:29:13 -0500

    -First Configuration

---
+++ application/controllers/Ajax_controller.php
@@ -0,0 +1,17 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Ajax_controller extends MY_Controller {
+
+	
+	public function index()
+	{
+		echo "index";
+	}
+}
+
+
+
+?>
+
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+class MY_Controller extends CI_Controller {
+	public function __construct() {
+		parent::__construct();
+		
+		$this->setLanguage();
+		$this->setTheme();
+	}
+
+	private function setLanguage() {
+		
+	}
+	
+	private function setTheme() {
+		echo "extends<br>";
+	}
+	
+}
+
+?>
