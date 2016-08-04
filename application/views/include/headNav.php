47d4f94531f8d6dd3eec9df5e837774cf484e7ea
summery <hellosummer0604@gmail.com>
2016-08-03 21:18:10 -0400

    -login panel opacity

--- application/views/include/headNav.php
+++ application/views/include/headNav.php
@@ -1,9 +1,15 @@
 <div class="headerNavBackground">
 
 </div>
+
 <nav class="headerNav">
     <span class="float_left bigLogo">North Gallery</span>
-    <span class="font_size_14 loginBtn">Log in</span>
-
-    <span class="font_size_14 signupBtn" id="upload">Sign up</span>
+	<span class="userBanner">
+    	<span class="font_size_14 loginBtn">Log in</span>
+    	<span class="font_size_14 signupBtn" id="upload">Sign up</span>
+	</span>
+	<span class="userBanner"  id="currentUser">
+    	<span class="font_size_14 signupBtn" id="upload">Log out</span>
+    	<span class="font_size_14 loginBtn">Hello Summery</span>
+	</span>
 </nav>
